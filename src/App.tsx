eea6faced2970fc71740b5c0a347adf00a15c22c
Ucrash <gducrash@gmail.com>
2023-01-01 00:21:47 +0200

    add GDRWeb to the app (test)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
+import { useState, useRef } from 'react';
+import { useSize } from 'ahooks';
+import { GDRWebCanvas } from 'gdrweb-react';
+
 import './App.scss';
 
 const App = () => {
+
+    const canvasWrapperRef = useRef<HTMLDivElement>(null);
+    const size = useSize(canvasWrapperRef);
+
+    const [ levelString, setLevelString ] = useState('');
+
     return (
         <div id="app">
-            <span>Greetings!</span>
+            <div className='canvasWrapper' ref={canvasWrapperRef}>
+                <GDRWebCanvas
+                    levelString={levelString}
+                    width={size?.width ?? 800}
+                    height={size?.height ?? 600}
+                    config={{
+                        pan: {
+                            mouseButtons: 'middle'
+                        }
+                    }}
+                />
+            </div>
+            <div>
+                <h2>Upload the Level file:</h2>
+                <input type="file" onChange={(e) => {
+                    const file = e.target.files?.item(0);
+                    if (file) {
+                        const reader = new FileReader();
+                        reader.onload = e => {
+                            if (e.target?.result)
+                                setLevelString(e.target.result.toString());
+                        };
+                        reader.readAsText(file);
+                    }
+                }} />
+            </div>
         </div>
     );
 };
