efc63125ed68f92f2c6ab1940e90d4a7e1cad362
Ucrash <gducrash@gmail.com>
2023-01-01 21:31:17 +0200

    setup global paths, add more icons

--- src/utils/icons/index.ts
+++ src/utils/icons/index.ts
@@ -1,7 +1,16 @@
 import { Icon } from '@iconify/react/dist/offline';
 
 import arrowRight from '@iconify-icons/tabler/chevron-right';
+import newFile from '@iconify-icons/tabler/file';
+import save from '@iconify-icons/tabler/device-floppy';
+import open from '@iconify-icons/tabler/folder';
 
-export default {
-    arrowRight
+const icons: any = {
+    arrowRight, 
+    newFile,
+    save, 
+    open,
 }
+
+export { Icon };
+export default icons;
