a1d47aaf3f02e76a31df64866b186f72d9f3b6eb
Ucrash <gducrash@gmail.com>
2023-01-02 17:23:33 +0200

    menubar menu fix

--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -69,6 +69,7 @@ const MenuBar = ({
                     <OptionsMenu
                         options={items[activeItem].options}
                         translucent
+                        key={activeItem}
                     />
                 </div>
             )}
