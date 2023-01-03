5d637d1e05d5f66210e8b5e0a018aa77ca8aba77
Ucrash <gducrash@gmail.com>
2023-01-03 18:05:27 +0200

    options menu now can't go offscreen

--- src/components/OptionsMenu/OptionsMenu.tsx
+++ src/components/OptionsMenu/OptionsMenu.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useLayoutEffect, useRef } from 'react';
 import cn from 'classnames';
 
 import Option, { MenuOption, MenuDivider, MenuList } from './components/Option';
@@ -7,6 +7,7 @@ import classes from './OptionsMenu.module.scss';
 
 
 const SUBMENU_Y_SHIFT = 8;
+const MENU_PADDING = 16;
 
 type OptionsMenuProps = {
     title?: string;
@@ -24,10 +25,49 @@ const OptionsMenu = ({
 
     const submenuActive = openSubmenu > -1;
 
+
+    const [ menuPosShift, setMenuPosShift ] = useState([0, 0]);
+    const [ menuXFlip, setMenuXFlip ] = useState(false);
+
+    const menuRef = useRef<HTMLDivElement>(null);
+
+    const calcMenuPosShift = () => {
+        if (!menuRef.current) return [0, 0];
+
+        const menuRect = menuRef.current.getBoundingClientRect();
+        const menuBottom = menuRect.top + menuRect.height;
+        const menuRight = menuRect.left + menuRect.width;
+        const windowBottom = window.innerHeight - MENU_PADDING;
+        const windowRight = window.innerWidth - MENU_PADDING;
+
+        return [
+            Math.max(0, menuRight - windowRight),
+            Math.max(0, menuBottom - windowBottom),
+        ];
+    }
+
+    const calcMenuXFlip = () => {
+        if (!menuRef.current) return false;
+
+        const menuRect = menuRef.current.getBoundingClientRect();
+        const menuRight = menuRect.left + menuRect.width;
+        const windowRight = window.innerWidth - MENU_PADDING - menuRect.width;
+
+        return menuRight > windowRight;
+    }
+
+    useLayoutEffect(() => {
+        setMenuPosShift(calcMenuPosShift());
+        setMenuXFlip(calcMenuXFlip());
+    }, []);
+
     return (
-        <div className={cn(classes.container, {
-            [classes.translucent]: translucent
-        })}>
+        <div ref={menuRef} className={cn(classes.container, {
+            [classes.translucent]: translucent,
+            [classes.xFlip]: menuXFlip,
+        })} style={{
+            transform: `translate(-${menuPosShift[0]}px, -${menuPosShift[1]}px)`
+        }}>
 
             {/** Menu Title */}
             {title && <h4>{title}</h4>}
